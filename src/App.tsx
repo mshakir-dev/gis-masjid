d3015752221ed7339238af862bb884deea0fbd31
Muhammad Shakir <mshakir.dev@gmail.com>
2025-09-14 23:44:23 -0500

    Use HashRouter to Support Github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, HashRouter as Router, Routes } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import { usePageTitle } from "./hooks/usePageTitle";
 
